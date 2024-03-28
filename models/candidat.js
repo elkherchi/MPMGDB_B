27823921e2df35b1666fdb5d2ac436beea65aa7f
cheikhbdh <22003@supnum.mr>
2024-03-28 01:44:02 +0000

    hhh

--- models/candidat.js
+++ models/candidat.js
@@ -1,9 +1,11 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const Candidat = mongoose.Schema({
-    nom: { type: String,  },
-    parti: { type: String,  },
-    circonscription:{ type: String,  },
-    profile : {type : String},
+const validationNniSchema = new mongoose.Schema({
+    nom: { type: String },
+    prenom: { type: String },
+    nni: { type: String },
+    sexe: { type: String, enum: ['M', 'F'] }, // Enum for Male and Female, adjust as needed
+    age: { type: Number }
 });
-module.exports = mongoose.model("candidat", Candidat)
+
+module.exports = mongoose.model("ValidationNni", validationNniSchema);
